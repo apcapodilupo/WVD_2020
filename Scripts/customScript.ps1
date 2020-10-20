1732e7dcbd78c8f7aa1c179c3a920520d1031630
acapodilupo <anthony.capodilupo@techdata.com>
2020-10-20 19:18:27 -0400

    Update customScript.ps1

--- Scripts/customScript.ps1
+++ Scripts/customScript.ps1
@@ -18,7 +18,7 @@ $connectionString = '\\' + $storageAccountName + '.file.core.windows.net\userpro
 #Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/apcapodilupo/WVD_2020/main/Scripts/install.ps1'))
 
-https://raw.githubusercontent.com/acapodilupo/WVD_Solution/master/DSC/chocolatey.0.10.15.nupkg
+
 
 #installs fslogix apps 
 choco install fslogix -yes --ignore-checksums
