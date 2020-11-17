13d2f09362cac6edf330a300f4d8f2702d0684f7
acapodilupo <anthony.capodilupo@techdata.com>
2020-11-17 14:34:40 -0500

    added both scripts for autodesk on WVDv3

---
+++ Scripts/Autodesk on WVDv3/ADDCCustomScript.ps1
@@ -0,0 +1,87 @@
+﻿Param(
+  [string] $storageAccountName,
+  [string] $ResourceGroupName,
+  [string] $administratorAccountUsername,
+  [string] $administratorAccountPassword,
+  [string] $SubscriptionId
+  )
+ 
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -force
+
+#enable TLS 1.2 (required for Windows Server)###############################################################################
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
+sleep 5
+
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
+sleep 5
+#################################################################################################################################
+
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module -Name PowerShellGet -Force -AllowClobber
+sleep 5
+
+#install AZ modules
+Install-Module -Name Az -force -AllowClobber
+sleep 30
+
+Import-Module Az.Accounts -force 
+sleep 30
+
+
+$Url = 'https://github.com/apcapodilupo/WVD_2020/blob/main/Scripts/JoinStorageAccount.zip?raw=true' 
+Invoke-WebRequest -Uri $Url -OutFile "C:\JoinStorageAccount.zip"
+Expand-Archive -Path "C:\JoinStorageAccount.zip" -DestinationPath "C:\JoinStorageAccount" -Force 
+
+#create share name
+$shareName = $storageAccountName+'.file.core.windows.net'
+$connectionString = '\\' + $storageAccountName + '.file.core.windows.net\userprofiles'
+###########Files#################################################################################################################
+
+##Install FSLOGIX Agent
+#sets execution policy to 'bypass' and installs chocolatey package manager
+#Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/apcapodilupo/WVD_2020/main/Scripts/install.ps1'))
+
+
+#installs fslogix apps 
+choco install fslogix -yes --ignore-checksums
+
+sleep 10
+
+
+#configure fslogix profile containers
+
+
+#create profiles key
+New-Item 'HKLM:\Software\FSLogix\Profiles' -Force 
+sleep 10
+
+#create enabled value
+New-ITEMPROPERTY 'HKLM:\Software\FSLogix\Profiles' -Name Enabled -Value 1
+sleep 10
+
+
+#removes any local profiles that are found
+New-ITEMPROPERTY 'HKLM:\Software\FSLogix\Profiles' -Name DeleteLocalProfileWhenVHDShouldApply -Value 1
+sleep 10
+
+#set  connection string
+New-ITEMPROPERTY 'HKLM:\Software\FSLogix\Profiles' -Name VHDLocations -PropertyType String -Value $connectionString
+sleep 10
+
+#set to vhdx
+New-ITEMPROPERTY 'HKLM:\Software\FSLogix\Profiles' -Name VolumeType -PropertyType String -Value "vhdx"
+
+sleep 10
+
+#enable GPU rendering
+New-ITEMPROPERTY 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -Name bEnumerateHWBeforeSW -Value 1
+sleep 10
+
+New-ITEMPROPERTY 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -Name AVCHardwareEncodePreferred -Value 1
+sleep 10
+
+New-ITEMPROPERTY 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -Name AVC444ModePreferred -Value 1
+sleep 10
+
+
