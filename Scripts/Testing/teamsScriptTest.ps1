539c1a74412e325ceb2276d3bdf4f394f7164d3c
acapodilupo <anthony.capodilupo@techdata.com>
2020-12-15 10:18:12 -0500

    uncommenting fslogix install for troubleshooting

--- Scripts/Testing/teamsScriptTest.ps1
+++ Scripts/Testing/teamsScriptTest.ps1
@@ -8,44 +8,44 @@ Param(
   )
 
 
-##create share name
-#$shareName = $storageAccountName+'.file.core.windows.net'
-#$connectionString = '\\' + $storageAccountName + '.file.core.windows.net\userprofiles'
-############Files#################################################################################################################
-#
-###Install FSLOGIX Agent
-##sets execution policy to 'bypass' and installs chocolatey package manager
-#Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/apcapodilupo/WVD_2020/main/Scripts/install.ps1'))
-#
-#
-##installs fslogix apps 
-#choco install fslogix -yes --ignore-checksums
-#
-#sleep 10
-#
-##configure fslogix profile containers
-#
-##create profiles key
-#New-Item 'HKLM:\Software\FSLogix\Profiles' -Force 
-#sleep 10
-#
-##create enabled value
-#New-ITEMPROPERTY 'HKLM:\Software\FSLogix\Profiles' -Name Enabled -Value 1
-#sleep 10
-#
-#
-##removes any local profiles that are found
-#New-ITEMPROPERTY 'HKLM:\Software\FSLogix\Profiles' -Name DeleteLocalProfileWhenVHDShouldApply -Value 1
-#sleep 10
-#
-##set  connection string
-#New-ITEMPROPERTY 'HKLM:\Software\FSLogix\Profiles' -Name VHDLocations -PropertyType String -Value $connectionString
-#sleep 10
-#
-##set to vhdx
-#New-ITEMPROPERTY 'HKLM:\Software\FSLogix\Profiles' -Name VolumeType -PropertyType String -Value "vhdx"
-#
-#sleep 10
+#create share name
+$shareName = $storageAccountName+'.file.core.windows.net'
+$connectionString = '\\' + $storageAccountName + '.file.core.windows.net\userprofiles'
+###########Files#################################################################################################################
+
+##Install FSLOGIX Agent
+#sets execution policy to 'bypass' and installs chocolatey package manager
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/apcapodilupo/WVD_2020/main/Scripts/install.ps1'))
+
+
+#installs fslogix apps 
+choco install fslogix -yes --ignore-checksums
+
+sleep 10
+
+#configure fslogix profile containers
+
+#create profiles key
+New-Item 'HKLM:\Software\FSLogix\Profiles' -Force 
+sleep 10
+
+#create enabled value
+New-ITEMPROPERTY 'HKLM:\Software\FSLogix\Profiles' -Name Enabled -Value 1
+sleep 10
+
+
+#removes any local profiles that are found
+New-ITEMPROPERTY 'HKLM:\Software\FSLogix\Profiles' -Name DeleteLocalProfileWhenVHDShouldApply -Value 1
+sleep 10
+
+#set  connection string
+New-ITEMPROPERTY 'HKLM:\Software\FSLogix\Profiles' -Name VHDLocations -PropertyType String -Value $connectionString
+sleep 10
+
+#set to vhdx
+New-ITEMPROPERTY 'HKLM:\Software\FSLogix\Profiles' -Name VolumeType -PropertyType String -Value "vhdx"
+
+sleep 10
 
 
 
