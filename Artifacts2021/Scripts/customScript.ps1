bc5b74ff6e92fae63efbf19e34ec00fcc4af828a
acapodilupo <anthony.capodilupo@techdata.com>
2021-01-25 14:57:33 -0500

    Added repo URIs

--- Artifacts2021/Scripts/customScript.ps1
+++ Artifacts2021/Scripts/customScript.ps1
@@ -4,9 +4,9 @@ Param(
   [string] $ResourceGroupName,
   [string] $administratorAccountUsername,
   [string] $administratorAccountPassword,
-  [string] $subscriptionID
+  [string] $subscriptionID,
+  [string] $installTeams
   )
-
 
 #create share name
 $shareName = $storageAccountName+'.file.core.windows.net'
@@ -49,4 +49,54 @@ sleep 10
 
 
 
+
+#Add Defender Exclusions for FSLogix
+powershell -Command "Add-MpPreference -ExclusionPath 'C:\Program Files\FSLogix\Apps\frxdrv.sys’"
+powershell -Command "Add-MpPreference -ExclusionPath 'C:\Program Files\FSLogix\Apps\frxdrvvt.sys’"
+powershell -Command "Add-MpPreference -ExclusionPath 'C:\Program Files\FSLogix\Apps\frxccd.sys’"
+powershell -Command "Add-MpPreference -ExclusionExtension '%TEMP%\*.VHD’"
+powershell -Command "Add-MpPreference -ExclusionExtension '%TEMP%\*.VHDX’"
+powershell -Command "Add-MpPreference -ExclusionExtension '%Windir%\*.VHD’"
+powershell -Command "Add-MpPreference -ExclusionExtension '%Windir%\*.VHDX’"
+powershell -Command "Add-MpPreference -ExclusionExtension '\\gcrwvduserprofiles.file.core.windows.net\userprofiles\*\*.*.VHDX’"
+powershell -Command "Add-MpPreference -ExclusionExtension '\\gcrwvduserprofiles.file.core.windows.net\userprofiles\*\*.*.VHD’"
+powershell -Command "Add-MpPreference -ExclusionProcess '%Program Files%\FSLogix\Apps\frxccd.exe’"
+powershell -Command "Add-MpPreference -ExclusionProcess '%Program Files%\FSLogix\Apps\frxccds.exe’"
+powershell -Command "Add-MpPreference -ExclusionProcess '%Program Files%\FSLogix\Apps\frxsvc.exe’"
+
+
+if ($installTeams -eq 'Yes'){
+
+    #create Teams folder in C drive
+    New-Item -Path "c:\" -Name "Install" -ItemType "directory"
+
+    # Add registry Key
+    reg add "HKLM\SOFTWARE\Microsoft\Teams" /v IsWVDEnvironment /t REG_DWORD /d 1 /f
+    sleep 5
+
+    #Download C++ Runtime
+    invoke-WebRequest -Uri https://aka.ms/vs/16/release/vc_redist.x64.exe -OutFile "C:\Install\vc_redist.x64.exe"
+    sleep 5
+
+    #Download RDCWEBRTCSvc
+    invoke-WebRequest -Uri https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RE4AQBt -OutFile "C:\Install\MsRdcWebRTCSvc_HostSetup_1.0.2006.11001_x64.msi"
+    sleep 5
+
+    #Download Teams 
+    invoke-WebRequest -Uri https://statics.teams.cdn.office.net/production-windows-x64/1.3.00.13565/Teams_windows_x64.msi -OutFile "C:\Install\Teams_windows_x64.msi"
+    sleep 5
+
+    #Install C++ runtime
+    Start-Process -FilePath C:\Install\vc_redist.x64.exe -ArgumentList '/q', '/norestart'
+    sleep 5
+
+    #Install Web Socket Redirector Service
+    msiexec /i C:\Install\MsRdcWebRTCSvc_HostSetup_1.0.2006.11001_x64.msi /q /n
+    sleep 5
+
+    # Install Teams
+    msiexec /i "C:\Install\Teams_windows_x64.msi" /l*v c:\Install\Teams.log ALLUSER=1 ALLUSERS=1 
+    sleep 5
+
+}
 
