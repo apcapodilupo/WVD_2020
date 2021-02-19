427f97858b0c856d40c298a4132736c7ddf806c9
acapodilupo <anthony.capodilupo@techdata.com>
2021-02-19 12:17:08 -0500

    Update CustomScriptDOMAIN.ps1

--- Artifacts2021/Scripts/CustomScriptDOMAIN.ps1
+++ Artifacts2021/Scripts/CustomScriptDOMAIN.ps1
@@ -158,6 +158,79 @@ New-ITEMPROPERTY 'HKLM:\Software\FSLogix\Profiles' -Name VolumeType -PropertyTyp
 sleep 05
 
 
+#Add Defender Exclusions for FSLogix
+try{
+    Add-Content C:\DeploymentLogs\log.txt "Setting Defender Exclusions for FSLogix. exit code is: $LASTEXITCODE"
+    powershell -Command "Add-MpPreference -ExclusionPath 'C:\Program Files\FSLogix\Apps\frxdrv.sys’"
+    powershell -Command "Add-MpPreference -ExclusionPath 'C:\Program Files\FSLogix\Apps\frxdrvvt.sys’"
+    powershell -Command "Add-MpPreference -ExclusionPath 'C:\Program Files\FSLogix\Apps\frxccd.sys’"
+    powershell -Command "Add-MpPreference -ExclusionExtension '%TEMP%\*.VHD’"
+    powershell -Command "Add-MpPreference -ExclusionExtension '%TEMP%\*.VHDX’"
+    powershell -Command "Add-MpPreference -ExclusionExtension '%Windir%\*.VHD’"
+    powershell -Command "Add-MpPreference -ExclusionExtension '%Windir%\*.VHDX’"
+    powershell -Command "Add-MpPreference -ExclusionExtension '\\gcrwvduserprofiles.file.core.windows.net\userprofiles\*\*.*.VHDX’"
+    powershell -Command "Add-MpPreference -ExclusionExtension '\\gcrwvduserprofiles.file.core.windows.net\userprofiles\*\*.*.VHD’"
+    powershell -Command "Add-MpPreference -ExclusionProcess '%Program Files%\FSLogix\Apps\frxccd.exe’"
+    powershell -Command "Add-MpPreference -ExclusionProcess '%Program Files%\FSLogix\Apps\frxccds.exe’"
+    powershell -Command "Add-MpPreference -ExclusionProcess '%Program Files%\FSLogix\Apps\frxsvc.exe’"
+
+
+}
+catch{
+    Add-Content C:\DeploymentLogs\log.txt "Error setting defender exclusions. exit code is: $LASTEXITCODE"
+
+}
+
+
+if ($installTeams -eq 'Yes'){
+
+    Add-Content C:\DeploymentLogs\log.txt "Installing Teams. exit code is: $LASTEXITCODE"
+
+
+    #create Teams folder in C drive
+    New-Item -Path "c:\" -Name "Install" -ItemType "directory"
+
+    # Add registry Key
+    reg add "HKLM\SOFTWARE\Microsoft\Teams" /v IsWVDEnvironment /t REG_DWORD /d 1 /f
+    sleep 5
+
+    #Download C++ Runtime
+    invoke-WebRequest -Uri https://aka.ms/vs/16/release/vc_redist.x64.exe -OutFile "C:\Install\vc_redist.x64.exe"
+    sleep 5
+
+    #Download RDCWEBRTCSvc
+    invoke-WebRequest -Uri https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RE4AQBt -OutFile "C:\Install\MsRdcWebRTCSvc_HostSetup_1.0.2006.11001_x64.msi"
+    sleep 5
+
+    #Download Teams 
+    invoke-WebRequest -Uri https://statics.teams.cdn.office.net/production-windows-x64/1.3.00.13565/Teams_windows_x64.msi -OutFile "C:\Install\Teams_windows_x64.msi"
+    sleep 5
+
+    #Install C++ runtime
+    Start-Process -FilePath C:\Install\vc_redist.x64.exe -ArgumentList '/q', '/norestart'
+    sleep 5
+
+    #Install Web Socket Redirector Service
+    msiexec /i C:\Install\MsRdcWebRTCSvc_HostSetup_1.0.2006.11001_x64.msi /q /n
+    sleep 5
+
+    # Install Teams
+    msiexec /i "C:\Install\Teams_windows_x64.msi" /l*v c:\Install\Teams.log ALLUSER=1 ALLUSERS=1 
+    sleep 5
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 if($LASTEXITCODE -ne 0){
 
     Add-Content C:\DeploymentLogs\log.txt "Execution finished with non-zero exit code of: $LASTEXITCODE. Please check the error log."
